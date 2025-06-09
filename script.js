3e3b62bdb146d6b1f60ede55c7b13c1273c0f53e
Eric Theuri <tericmail001@gmail.com>
2025-06-09 20:28:53 +0300

    Modified AI

---
+++ script.js
@@ -0,0 +1,86 @@
+///////Chatbot Section///////              ///////Chatbot Section////
+const context = `Monturi was founded by Eric Theuri in the year 2025 June. He identified a problem, increasing number of online scams related to crypto and the desire for people to understand and invest in the financial world of forex, crypto and other financial instruments in decentralised finance. 
+Monturi's mission is to empower today’s generation in making wise financial decisions for a secure and opportunity-rich future.
+The Services/products we provide are: 
+    1.Fintech consulting for startups and other businesses in the financial sector
+    2. Roboadvisory whereby we do extensive market research on crypto and provide you, the client, with relevant first-hand information to enable you invest wisely.
+    3.A financial dashboard that helps you track all your invested assets from one point
+The team details:
+    1. Eric Theuri-Managing Director
+    2. Aisha Barasa-Chief Technical Officer
+    3. Benson Mose- Senior Relationship Managaer
+    4. Polycarp Irungu - Human Resource
+
+Problem Monturi Solves:
+    1. Online scams related to crypto
+    2. Inadequate knowledge regarding crypto and such instrument on the client cide
+Team contacts/Support:
+    Reach out to the company direct line : +254711366000 or via email monturifinance@gmail.com
+Vision/Long-term goal:
+    1.To be a leading force in transforming how people engage with money
+    2. Educate people on matters regarding crypto and blockchain
+
+
+ `
+ var questions = `1. Who founded the startup?
+ 2. What problem does it solve?
+ What services or products does it offer?
+ How can someone support or contact the team?
+ What's the startup's vision or long-term goal?
+ `
+
+
+
+
+
+
+
+
+ 
+ const chatMessagesContainer = document.getElementById('chat-messages');
+ const prompt = document.getElementById('user-input');
+ const send = document.getElementById("send-button");
+ const allSampleQuestionsButtons = document.querySelectorAll(".sample-button")
+///This is code to update the conversation field
+ function addMessage(text, sender) {
+    const messageDiv = document.createElement('div');
+    messageDiv.classList.add('message', sender);
+    const p =document.createElement('p');
+    p.textContent = text;
+    messageDiv.appendChild(p)
+    chatMessagesContainer.appendChild(messageDiv);
+ }
+ async function sendMessage(params) {//To create a continuous cycle of sending and receiving messages
+    const messageText = userInput.value.trim()//Gets the user input/prompt and trims it
+    //Now to evaluate if the user input is okay
+    if (messageText === '') {
+        addMessage("Ask a question about Monturi :)")
+        return;
+    }
+    addMessage(messageText, 'user');//Shows the user's prompt in the chat area(like a bubble convo)
+    userInput.value = ''; //clear the input field
+    try {
+        const response = await puter.ai.chat(messageText, {system : context}
+        );
+        if (response && response.content) {
+            addMessage(response.content, 'bot'); //This now shows the bot response via AI
+        }
+        else {
+            addMessage("I am unable to answer that at the momment ):")
+        }
+    }catch (error) {
+       addMessage("Servers are busy! Please try again...") ///Incase something goes wrong and the chatbot is unable to work as expected we display this error message
+    }
+    
+ }
+
+ if (allSampleQuestionsButtons.length > 0) {
+    allSampleQuestionsButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const question = button.CDATA_SECTION_NODE.question;
+            userInput.value = question;
+            sendMessage()
+        });
+    });
+
+ }
